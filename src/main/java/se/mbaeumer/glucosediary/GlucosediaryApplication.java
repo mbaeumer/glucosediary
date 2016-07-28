747cc7e004768621e86f90d944238c412d0a72fb
Martin Bäumer <martinbaumer@martins-air.lan>
2016-07-28 11:16:09 +0200

    creating a basic file structure

---
+++ src/main/java/se/mbaeumer/glucosediary/GlucosediaryApplication.java
@@ -0,0 +1,12 @@
+package se.mbaeumer.glucosediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucosediaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlucosediaryApplication.class, args);
+	}
+}
---
+++ src/main/java/se/mbaeumer/glucosediary/models/UserType.java
@@ -0,0 +1,11 @@
+package se.mbaeumer.glucosediary.models;
+
+/**
+ * Created by martinbaumer on 27/07/16.
+ */
+public class UserType {
+    private int id;
+    private String name;
+    private String description;
+
+}
