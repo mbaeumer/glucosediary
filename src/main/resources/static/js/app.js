ef8a523f5c1d5b1cebbe89b25eca511f56866efe
Martin Bäumer <martin.baeumer@gmail.com>
2016-07-29 22:25:38 +0200

    adding support for AngularJS

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('app', ['ngRoute','ngResource']);
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/users',{
+            templateUrl: '/views/users.html',
+            controller: 'usersController'
+        })
+        .when('/glucose',{
+            templateUrl: '/views/glucosemeasurements.html',
+            controller: 'rolesController'
+        })
+        .otherwise(
+            { redirectTo: '/'}
+        );
+});
