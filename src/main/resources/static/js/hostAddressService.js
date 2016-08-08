5076f0e3e28e5786e1f2617a3cfa72e375c21537
Martin Bäumer <martin.baeumer@gmail.com>
2016-08-08 21:57:33 +0200

    Updated hostAddress

--- src/main/resources/static/js/hostAddressService.js
+++ src/main/resources/static/js/hostAddressService.js
@@ -1,7 +1,7 @@
 var services = angular.module('services');
 services.factory('hostAddressService', function(){
     return {
-        hostAddress : 'glucometriq.herokuapp.com/', //'http://localhost:9090/',
+        hostAddress : 'http://localhost:9090/',
         testfunction : function(){
             var test1 = 'test1';
             var test2 = 'test2';
