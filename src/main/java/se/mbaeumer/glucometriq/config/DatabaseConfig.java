1e4139ca2497cc4045794accb588db1b591d5bfd
Martin Bäumer <martin.baeumer@gmail.com>
2016-08-16 19:23:03 +0200

    added properties for the datasource in heroku

--- src/main/java/se/mbaeumer/glucometriq/config/DatabaseConfig.java
+++ src/main/java/se/mbaeumer/glucometriq/config/DatabaseConfig.java
@@ -1,23 +1,23 @@
-//package se.mbaeumer.glucometriq.config;
-//
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//
-//import javax.sql.DataSource;
-//
-///**
-// * Created by martinbaumer on 15/08/16.
-// */
-//@Configuration
-//public class DatabaseConfig {
-//    @Bean
-//    @Primary
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource dataSource(){
-//        return DataSourceBuilder.create().build();
-//
-//    }
-//}
+package se.mbaeumer.glucometriq.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by martinbaumer on 15/08/16.
+ */
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+
+    }
+}
