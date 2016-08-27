9fca1842a9d6e511cfee037989101eec27a5c3dd
Martin Bäumer <martin.baeumer@gmail.com>
2016-08-27 22:02:21 +0200

    added settings for heroku

--- src/main/resources/static/js/hostAddressService.js
+++ src/main/resources/static/js/hostAddressService.js
@@ -1,7 +1,7 @@
 var services = angular.module('services');
 services.factory('hostAddressService', function(){
     return {
-        hostAddress : 'http://localhost:9090/',
+        hostAddress : 'http://glucometriq.herokupp.com/',
         testfunction : function(){
             var test1 = 'test1';
             var test2 = 'test2';
