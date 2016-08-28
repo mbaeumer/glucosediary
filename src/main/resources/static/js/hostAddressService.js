d41fd855c4f43890b46149d48224d039b8215007
Martin Bäumer <martin.baeumer@gmail.com>
2016-08-28 03:13:47 +0200

    trying to fix cors

--- src/main/resources/static/js/hostAddressService.js
+++ src/main/resources/static/js/hostAddressService.js
@@ -1,7 +1,7 @@
 var services = angular.module('services');
 services.factory('hostAddressService', function(){
     return {
-        hostAddress : 'http://glucometriq.herokupp.com/',
+        hostAddress : 'http://glucometriq.herokuapp.com/',
         testfunction : function(){
             var test1 = 'test1';
             var test2 = 'test2';
