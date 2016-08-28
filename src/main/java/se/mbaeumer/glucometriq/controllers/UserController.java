ca3634e7d0482507405427509eb3cb60070912e3
Martin Bäumer <martin.baeumer@gmail.com>
2016-08-28 03:18:47 +0200

    removed cors annotation from usercontroller

--- src/main/java/se/mbaeumer/glucometriq/controllers/UserController.java
+++ src/main/java/se/mbaeumer/glucometriq/controllers/UserController.java
@@ -56,7 +56,7 @@ public class UserController {
     }
     */
 
-    @CrossOrigin(origins = "http://glucometriq.herokuapp.com")
+    //@CrossOrigin(origins = "http://glucometriq.herokuapp.com")
     @RequestMapping(value="login", method=RequestMethod.POST)
     public User authenticate(@RequestBody Credentials credentials){
         User user = null;
